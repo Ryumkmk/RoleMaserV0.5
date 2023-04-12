372eab874f28086a5aeb5e9cdb570a78fe58ca97
Ryumkmk <luihao1999929@icloud.com>
2023-04-12 11:39:51 +0900

    generageHTML

---
+++ app/controllers/server.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
