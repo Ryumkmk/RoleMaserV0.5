6bee273ecfe4cc14a075cd5ef91518d71b05052f
Ryumkmk <luihao1999929@icloud.com>
2023-04-12 11:57:25 +0900

    server

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -21,6 +21,6 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("index", index)
-
+	fmt.Println("Stated Server")
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
