5df7b2d8fca9eca5252a0825a7dabade7b5e82b8
Ryumkmk <luihao1999929@icloud.com>
2023-04-14 18:37:00 +0900

    file

--- app/controllers/rout_file.go
+++ app/controllers/rout_file.go
@@ -1,1 +1,33 @@
 package controllers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile("./xlsx/"+filepath.Base(header.Filename), os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(f, file)
+
+}
+
+func showFile(w http.ResponseWriter, r *http.Request) {
+
+}
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -21,7 +21,7 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/top", top)
-
+	http.HandleFunc("/upload", upload)
 	fmt.Println("Stated Server")
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
