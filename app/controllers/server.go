07ace3216019ed9d4bcc0c2edc2576a7bada2c7f
Ryumkmk <luihao1999929@icloud.com>
2023-04-17 09:39:52 +0900

    style

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -20,6 +20,7 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/top", top)
 	http.HandleFunc("/upload", upload)
 	http.HandleFunc("/delete", delete)
