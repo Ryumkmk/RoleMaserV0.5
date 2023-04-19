841780eb6247839ef9dc2bbeeb1b00d31e934cf2
Ryumkmk <luihao1999929@icloud.com>
2023-04-19 23:19:40 +0900

    scc

--- app/views/js/scripts.js
+++ app/views/js/scripts.js
@@ -28,36 +28,36 @@
 //   console.log('fileInputまたはfileInputLabelが見つかりませんでした');
 // }
 
-function copyValue(input, targetName, copyName) {
-    var targetInput = input.closest(".AM").querySelectorAll("[name='" + targetName + "']")[0];
-    var copyInput = input.closest(".AM").querySelectorAll("[name='" + copyName + "']")[0];
-    targetInput.value = input.value;
-    copyInput.value = input.value;
-}
+// function copyValue(input, targetName, copyName) {
+//     var targetInput = input.closest(".AM").querySelectorAll("[name='" + targetName + "']")[0];
+//     var copyInput = input.closest(".AM").querySelectorAll("[name='" + copyName + "']")[0];
+//     targetInput.value = input.value;
+//     copyInput.value = input.value;
+// }
 
-function copyValue2(input, targetName, copyName) {
-    var targetInput = input.closest(".PM").querySelectorAll("[name='" + targetName + "']")[0];
-    var copyInput = input.closest(".PM").querySelectorAll("[name='" + copyName + "']")[0];
-    targetInput.value = input.value;
-    copyInput.value = input.value;
-}
+// function copyValue2(input, targetName, copyName) {
+//     var targetInput = input.closest(".PM").querySelectorAll("[name='" + targetName + "']")[0];
+//     var copyInput = input.closest(".PM").querySelectorAll("[name='" + copyName + "']")[0];
+//     targetInput.value = input.value;
+//     copyInput.value = input.value;
+// }
 
-function confirmDelete(event) {
-    event.preventDefault();
-    if (confirm("本当に削除しますか？")) {
-        // 削除処理
-        document.getElementById("delete-form").submit();
-    }
-}
+// function confirmDelete(event) {
+//     event.preventDefault();
+//     if (confirm("本当に削除しますか？")) {
+//         // 削除処理
+//         document.getElementById("delete-form").submit();
+//     }
+// }
 
 // input要素を取得
-const input2 = document.querySelector('input[name="drinkmain"]');
+const input = document.querySelector('input[name="drinkmain"]');
 
 // pjs-item要素を取得
 const items = document.querySelectorAll('.pjs-item');
 
 // inputの入力値が変更されたら
-input2.addEventListener('input', function (event) {
+input.addEventListener('input', function (event) {
     // 入力値を取得
     const inputValue = event.target.value;
     console.log(inputValue);
