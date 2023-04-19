507db0e427f9e1fa1b0ac2c883fd51459da6417b
Ryumkmk <luihao1999929@icloud.com>
2023-04-19 23:20:38 +0900

    sc ok?

--- app/views/js/scripts.js
+++ app/views/js/scripts.js
@@ -28,50 +28,50 @@
 //   console.log('fileInputまたはfileInputLabelが見つかりませんでした');
 // }
 
-// function copyValue(input, targetName, copyName) {
-//     var targetInput = input.closest(".AM").querySelectorAll("[name='" + targetName + "']")[0];
-//     var copyInput = input.closest(".AM").querySelectorAll("[name='" + copyName + "']")[0];
-//     targetInput.value = input.value;
-//     copyInput.value = input.value;
-// }
+function copyValue(input, targetName, copyName) {
+    var targetInput = input.closest(".AM").querySelectorAll("[name='" + targetName + "']")[0];
+    var copyInput = input.closest(".AM").querySelectorAll("[name='" + copyName + "']")[0];
+    targetInput.value = input.value;
+    copyInput.value = input.value;
+}
 
-// function copyValue2(input, targetName, copyName) {
-//     var targetInput = input.closest(".PM").querySelectorAll("[name='" + targetName + "']")[0];
-//     var copyInput = input.closest(".PM").querySelectorAll("[name='" + copyName + "']")[0];
-//     targetInput.value = input.value;
-//     copyInput.value = input.value;
-// }
+function copyValue2(input, targetName, copyName) {
+    var targetInput = input.closest(".PM").querySelectorAll("[name='" + targetName + "']")[0];
+    var copyInput = input.closest(".PM").querySelectorAll("[name='" + copyName + "']")[0];
+    targetInput.value = input.value;
+    copyInput.value = input.value;
+}
 
-// function confirmDelete(event) {
-//     event.preventDefault();
-//     if (confirm("本当に削除しますか？")) {
-//         // 削除処理
-//         document.getElementById("delete-form").submit();
-//     }
-// }
-
-// input要素を取得
-const input = document.querySelector('input[name="drinkmain"]');
-
-// pjs-item要素を取得
-const items = document.querySelectorAll('.pjs-item');
-
-// inputの入力値が変更されたら
-input.addEventListener('input', function (event) {
-    // 入力値を取得
-    const inputValue = event.target.value;
-    console.log(inputValue);
-    // pjs-item要素をループ
-    for (const item of items) {
-        // itemの名前を取得
-        const itemName = item.querySelector('.Names').textContent;
-        // itemNameがinputValueを含むかどうかを判定
-        if (itemName.includes(inputValue)) {
-            // 背景色を変更
-            item.style.backgroundColor = 'yellow';
-        } else {
-            // 背景色を初期化
-            item.style.backgroundColor = 'red';
-        }
+function confirmDelete(event) {
+    event.preventDefault();
+    if (confirm("本当に削除しますか？")) {
+        // 削除処理
+        document.getElementById("delete-form").submit();
     }
-});
+}
+
+// // input要素を取得
+// const input2 = document.querySelector('input[name="drinkmain"]');
+
+// // pjs-item要素を取得
+// const items = document.querySelectorAll('.pjs-item');
+
+// // inputの入力値が変更されたら
+// input2.addEventListener('input', function (event) {
+//     // 入力値を取得
+//     const inputValue = event.target.value;
+//     console.log(inputValue);
+//     // pjs-item要素をループ
+//     for (const item of items) {
+//         // itemの名前を取得
+//         const itemName = item.querySelector('.Names').textContent;
+//         // itemNameがinputValueを含むかどうかを判定
+//         if (itemName.includes(inputValue)) {
+//             // 背景色を変更
+//             item.style.backgroundColor = 'yellow';
+//         } else {
+//             // 背景色を初期化
+//             item.style.backgroundColor = 'red';
+//         }
+//     }
+// });
