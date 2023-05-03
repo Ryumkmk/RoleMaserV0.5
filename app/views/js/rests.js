836f108c05e51d92078d60918442b9bd881575cd
Ryumkmk <luihao1999929@icloud.com>
2023-05-03 22:48:19 +0900

    休憩の複数登録の改善

--- app/views/js/rests.js
+++ app/views/js/rests.js
@@ -1,94 +1,117 @@
 // towel2Pの文字列を取得する
-const subpj = document.querySelector(".subpj").firstChild.textContent.trim();
-const leader = document.querySelector(".leader").firstChild.textContent.trim();
-const returnleader = document.querySelector(".returnleader").firstChild.textContent.trim();
-const gatekeeper = document.querySelector(".gatekeeper").firstChild.textContent.trim();
-const parkkeeper1 = document.querySelector(".parkkeeper1").firstChild.textContent.trim();
-const parkkeeper2 = document.querySelector(".parkkeeper2").firstChild.textContent.trim();
-const cloak = document.querySelector(".cloak").firstChild.textContent.trim();
-const cloaksub = document.querySelector(".cloaksub").firstChild.textContent.trim();
-const coffee = document.querySelector(".coffee").firstChild.textContent.trim();
-const drinkmain = document.querySelector(".drinkmain").firstChild.textContent.trim();
-const drinksub = document.querySelector(".drinksub").firstChild.textContent.trim();
-const drinksubsub = document.querySelector(".drinksubsub").firstChild.textContent.trim();
-const champagne1 = document.querySelector(".champagne1").firstChild.textContent.trim();
-const champagne2 = document.querySelector(".champagne2").firstChild.textContent.trim();
-const silver1 = document.querySelector(".silver1").firstChild.textContent.trim();
-const silver2 = document.querySelector(".silver2").firstChild.textContent.trim();
-const ape1 = document.querySelector(".ape1").firstChild.textContent.trim();
-const ape2 = document.querySelector(".ape2").firstChild.textContent.trim();
-const toiletman = document.querySelector(".toiletman").firstChild.textContent.trim();
-const toiletlady = document.querySelector(".toiletlady").firstChild.textContent.trim();
-// const wash1 = document.querySelector(".wash1").firstChild.textContent.trim();
-// const wash2 = document.querySelector(".wash2").firstChild.textContent.trim();
+var existName = [];
+let subpj = document.querySelector(".subpj").firstChild.textContent.trim();
+existName.push(subpj);
+let leader = isExistName(document.querySelector(".leader").firstChild.textContent.trim());
+let returnleader = isExistName(document.querySelector(".returnleader").firstChild.textContent.trim());
+let gatekeeper = isExistName(document.querySelector(".gatekeeper").firstChild.textContent.trim());
+let parkkeeper1 = isExistName(document.querySelector(".parkkeeper1").firstChild.textContent.trim());
+let parkkeeper2 = isExistName(document.querySelector(".parkkeeper2").firstChild.textContent.trim());
+let cloak = isExistName(document.querySelector(".cloak").firstChild.textContent.trim());
+let cloaksub = isExistName(document.querySelector(".cloaksub").firstChild.textContent.trim());
+let coffee = isExistName(document.querySelector(".coffee").firstChild.textContent.trim());
+let drinkmain = isExistName(document.querySelector(".drinkmain").firstChild.textContent.trim());
+let drinksub = isExistName(document.querySelector(".drinksub").firstChild.textContent.trim());
+let drinksubsub = isExistName(document.querySelector(".drinksubsub").firstChild.textContent.trim());
+let champagne1 = isExistName(document.querySelector(".champagne1").firstChild.textContent.trim());
+let champagne2 = isExistName(document.querySelector(".champagne2").firstChild.textContent.trim());
+let silver1 = isExistName(document.querySelector(".silver1").firstChild.textContent.trim());
+let silver2 = isExistName(document.querySelector(".silver2").firstChild.textContent.trim());
+let ape1 = isExistName(document.querySelector(".ape1").firstChild.textContent.trim());
+let ape2 = isExistName(document.querySelector(".ape2").firstChild.textContent.trim());
+let toiletman = isExistName(document.querySelector(".toiletman").firstChild.textContent.trim());
+let toiletlady = isExistName(document.querySelector(".toiletlady").firstChild.textContent.trim());
+// let wash1 = isExistName(document.querySelector(".wash1").firstChild.textContent.trim());
+// let wash2 = isExistName(document.querySelector(".wash2").firstChild.textContent.trim());
 
 
 // rest要素内のp要素に書き込む
-const restsubpj = document.querySelector(".rest-subpj");
+let restsubpj = document.querySelector(".rest-subpj");
 restsubpj.textContent += subpj;
-const restleader = document.querySelector(".rest-leader");
+let restleader = document.querySelector(".rest-leader");
 restleader.textContent += leader;
 // restleader.textContent += wash2;
-const restgatekeeper = document.querySelector(".rest-gatekeeper");
+let restgatekeeper = document.querySelector(".rest-gatekeeper");
 restgatekeeper.textContent += (gatekeeper || "") + (gatekeeper && (parkkeeper1 || parkkeeper2) ? "," : "") + (parkkeeper1 || "") + (parkkeeper1 && parkkeeper2 ? "," : "") + (parkkeeper2 || "");
-const restcloak = document.querySelector(".rest-cloak");
+let restcloak = document.querySelector(".rest-cloak");
 restcloak.textContent += (cloak || "") + (cloak && (returnleader || cloaksub) ? "," : "") + (returnleader || "") + (returnleader && cloaksub ? "," : "") + (cloaksub || "");
-const restcoffee = document.querySelector(".rest-coffee");
+let restcoffee = document.querySelector(".rest-coffee");
 restcoffee.textContent += coffee;
 // restcoffee.textContent += wash1;
-const restdrink = document.querySelector(".rest-drink");
+let restdrink = document.querySelector(".rest-drink");
 restdrink.textContent += (drinkmain || "") + (drinkmain && (drinksub || drinksubsub) ? "," : "") + (drinksub || "") + (drinksub && drinksubsub ? "," : "") + (drinksubsub || "");
-const restchampagne = document.querySelector(".rest-champagne");
+let restchampagne = document.querySelector(".rest-champagne");
 restchampagne.textContent += (champagne1 || "") + (champagne1 && (champagne2 || toiletlady) ? "," : "") + (champagne2 || "") + (champagne2 && toiletlady ? "," : "") + (toiletlady || "");
-const restsilver = document.querySelector(".rest-silver");
+let restsilver = document.querySelector(".rest-silver");
 restsilver.textContent += (silver1 || "") + (silver1 && silver2 ? "," : "") + (silver2 || "");
-const restape = document.querySelector(".rest-ape");
+let restape = document.querySelector(".rest-ape");
 restape.textContent += (ape1 || "") + (ape1 && (ape2 || toiletman) ? "," : "") + (ape2 || "") + (ape2 && toiletman ? "," : "") + (toiletman || "");
 
-
 //PM
+var existNameP = [];
+let subpjP = isExistNameP(document.querySelector(".subpjP").firstChild.textContent.trim());
+existNameP.push(subpjP);
+let leaderP = isExistNameP(document.querySelector(".leaderP").firstChild.textContent.trim());
+let returnleaderP = isExistNameP(document.querySelector(".returnleaderP").firstChild.textContent.trim());
+let gatekeeperP = isExistNameP(document.querySelector(".gatekeeperP").firstChild.textContent.trim());
+let parkkeeper1P = isExistNameP(document.querySelector(".parkkeeper1P").firstChild.textContent.trim());
+let parkkeeper2P = isExistNameP(document.querySelector(".parkkeeper2P").firstChild.textContent.trim());
+let cloakP = isExistNameP(document.querySelector(".cloakP").firstChild.textContent.trim());
+let cloaksubP = isExistNameP(document.querySelector(".cloaksubP").firstChild.textContent.trim());
+let coffeeP = isExistNameP(document.querySelector(".coffeeP").firstChild.textContent.trim());
+let drinkmainP = isExistNameP(document.querySelector(".drinkmainP").firstChild.textContent.trim());
+let drinksubP = isExistNameP(document.querySelector(".drinksubP").firstChild.textContent.trim());
+let drinksubsubP = isExistNameP(document.querySelector(".drinksubsubP").firstChild.textContent.trim());
+let champagne1P = isExistNameP(document.querySelector(".champagne1P").firstChild.textContent.trim());
+let champagne2P = isExistNameP(document.querySelector(".champagne2P").firstChild.textContent.trim());
+let silver1P = isExistNameP(document.querySelector(".silver1P").firstChild.textContent.trim());
+let silver2P = isExistNameP(document.querySelector(".silver2P").firstChild.textContent.trim());
+let ape1P = isExistNameP(document.querySelector(".ape1P").firstChild.textContent.trim());
+let ape2P = isExistNameP(document.querySelector(".ape2P").firstChild.textContent.trim());
+let toiletmanP = isExistNameP(document.querySelector(".toiletmanP").firstChild.textContent.trim());
+let toiletladyP = isExistNameP(document.querySelector(".toiletladyP").firstChild.textContent.trim());
+// let wash1P = isExistName(document.querySelector(".wash1P").firstChild.textContent.trim());
+// let wash2P = isExistName(document.querySelector(".wash2P").firstChild.textContent.trim());
 
-const subpjP = document.querySelector(".subpjP").firstChild.textContent.trim();
-const leaderP = document.querySelector(".leaderP").firstChild.textContent.trim();
-const returnleaderP = document.querySelector(".returnleaderP").firstChild.textContent.trim();
-const gatekeeperP = document.querySelector(".gatekeeperP").firstChild.textContent.trim();
-const parkkeeper1P = document.querySelector(".parkkeeper1P").firstChild.textContent.trim();
-const parkkeeper2P = document.querySelector(".parkkeeper2P").firstChild.textContent.trim();
-const cloakP = document.querySelector(".cloakP").firstChild.textContent.trim();
-const cloaksubP = document.querySelector(".cloaksubP").firstChild.textContent.trim();
-const coffeeP = document.querySelector(".coffeeP").firstChild.textContent.trim();
-const drinkmainP = document.querySelector(".drinkmainP").firstChild.textContent.trim();
-const drinksubP = document.querySelector(".drinksubP").firstChild.textContent.trim();
-const drinksubsubP = document.querySelector(".drinksubsubP").firstChild.textContent.trim();
-const champagne1P = document.querySelector(".champagne1P").firstChild.textContent.trim();
-const champagne2P = document.querySelector(".champagne2P").firstChild.textContent.trim();
-const silver1P = document.querySelector(".silver1P").firstChild.textContent.trim();
-const silver2P = document.querySelector(".silver2P").firstChild.textContent.trim();
-const ape1P = document.querySelector(".ape1P").firstChild.textContent.trim();
-const ape2P = document.querySelector(".ape2P").firstChild.textContent.trim();
-const toiletmanP = document.querySelector(".toiletmanP").firstChild.textContent.trim();
-const toiletladyP = document.querySelector(".toiletladyP").firstChild.textContent.trim();
-// const wash1P = document.querySelector(".wash1P").firstChild.textContent.trim();
-// const wash2P = document.querySelector(".wash2P").firstChild.textContent.trim();
-
-const restsubpjP = document.querySelector(".rest-subpjP");
+let restsubpjP = document.querySelector(".rest-subpjP");
 restsubpjP.textContent += subpjP;
-const restleaderP = document.querySelector(".rest-leaderP");
+let restleaderP = document.querySelector(".rest-leaderP");
 restleaderP.textContent += leaderP;
 // restleaderP.textContent += wash2P;
-const restgatekeeperP = document.querySelector(".rest-gatekeeperP");
+let restgatekeeperP = document.querySelector(".rest-gatekeeperP");
 restgatekeeperP.textContent += (gatekeeperP || "") + (gatekeeperP && (parkkeeper1P || parkkeeper2P) ? "," : "") + (parkkeeper1P || "") + (parkkeeper1P && parkkeeper2P ? "," : "") + (parkkeeper2P || "");
-const restcloakP = document.querySelector(".rest-cloakP");
+let restcloakP = document.querySelector(".rest-cloakP");
 restcloakP.textContent += (cloakP || "") + (cloakP && (returnleaderP || cloaksubP) ? "," : "") + (returnleaderP || "") + (returnleaderP && cloaksubP ? "," : "") + (cloaksubP || "");
-const restcoffeeP = document.querySelector(".rest-coffeeP");
+let restcoffeeP = document.querySelector(".rest-coffeeP");
 restcoffeeP.textContent += coffeeP;
 // restcoffeeP.textContent += wash1P;
-const restdrinkP = document.querySelector(".rest-drinkP");
+let restdrinkP = document.querySelector(".rest-drinkP");
 restdrinkP.textContent += (drinkmainP || "") + (drinkmainP && (drinksubP || drinksubsubP) ? "," : "") + (drinksubP || "") + (drinksubP && drinksubsubP ? "," : "") + (drinksubsubP || "");
-const restchampagneP = document.querySelector(".rest-champagneP");
+let restchampagneP = document.querySelector(".rest-champagneP");
 restchampagneP.textContent += (champagne1P || "") + (champagne1P && (champagne2P || toiletladyP) ? "," : "") + (champagne2P || "") + (champagne2P && toiletladyP ? "," : "") + (toiletladyP || "");
-const restsilverP = document.querySelector(".rest-silverP");
+let restsilverP = document.querySelector(".rest-silverP");
 restsilverP.textContent += (silver1P || "") + (silver1P && silver2P ? "," : "") + (silver2P || "");
-const restapeP = document.querySelector(".rest-apeP");
+let restapeP = document.querySelector(".rest-apeP");
 restapeP.textContent += (ape1P || "") + (ape1P && (ape2P || toiletmanP) ? "," : "") + (ape2P || "") + (ape2P && toiletmanP ? "," : "") + (toiletmanP || "");
+console.log(existName);
+console.log(existNameP);
 
+function isExistName(obj) {
+    if (existName.indexOf(obj) !== -1) {
+        // console.log('存在します')
+        return ""
+    } else {
+        existName.push(obj);
+        return obj
+    }
+}
+
+function isExistNameP(obj) {
+    if (existNameP.indexOf(obj) !== -1) {
+        // console.log('存在します')
+        return ""
+    } else {
+        existNameP.push(obj);
+        return obj
+    }
+}
