3e53247db587e8821cca33ff0753687d96afba6c
Ryumkmk <luihao1999929@icloud.com>
2023-05-31 17:20:32 +0900

    updated

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import "RMV0.5/app/models"
+import "RMV0.5/app/controllers"
 
 func main() {
 
-	// controllers.StartMainServer()
-	models.InsertAllRowsShifts("5月シフト")
+	controllers.StartMainServer()
+	// models.InsertAllRowsShifts("5月シフト")
 }
